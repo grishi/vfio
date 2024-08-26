c7303e571977bdf722b18934f7dfc4f55baa56da
grishi <g-github@grishi.com>
2024-08-26 16:16:23 -0400

    updated KVM hooks

--- revert.sh
+++ revert.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 set -x
 
+echo "Stopping coolercontrold.service..."
 systemctl stop coolercontrold.service
 
-# Unload VFIO-PCI Kernel Driver
+echo "Unloading VFIO-PCI Kernel Drivers..."
 modprobe -r vfio_pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
 
-# Re-Bind GPU to our display drivers
+echo "Re-Binding GPU to our display drivers..."
 virsh nodedev-reattach pci_0000_01_00_0
 virsh nodedev-reattach pci_0000_01_00_1
 
-# Rebind VT consoles
+#echo "Rebinding VT consoles..."
 #echo 1 > /sys/class/vtconsole/vtcon0/bind
 #echo 1 > /sys/class/vtconsole/vtcon1/bind
-# Re-Bind EFI-Framebuffer
+
+#echo "Rebinding EFI-Framebuffer..."
 #echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/bind
 
-# Read our nvidia configuration when before starting our graphics
+echo "Reading NVIDIA configuration..."
 nvidia-xconfig --query-gpu-info > /dev/null 2>&1
 
-# Load nvidia drivers
+echo "Loading NVIDIA drivers..."
 modprobe nvidia_drm
 modprobe nvidia_modeset
 modprobe nvidia_uvm
 modprobe i2c_nvidia_gpu
 modprobe nvidia
 
+echo "Mounting filesystems..."
+mount -a
+echo "Starting services..."
 # Restart Display Manager
 #systemctl start sddm.service
-mount -a
+echo "Starting nvidia-persistenced.service..."
 systemctl start nvidia-persistenced.service
 #nvidia-smi -pl 360
+echo "Starting coolercontrold.service..."
 systemctl start coolercontrold.service
+echo "Starting ollama.service..."
 systemctl start ollama.service
+echo "Starting comfyui.service..."
 systemctl start comfyui.service
 
+#echo "Move back to workspace 1 on Hyprland..."
 #hyprctl dispatch workspace 1
 
+#echo "Set LGTV to input 1..."
 #lgtv setInput HDMI_1
-
-## Another way
-#echo 0000:01:00.0 > /sys/bus/pci/devices/0000:01:00.0/driver/unbind
-#echo 0000:01:00.1 > /sys/bus/pci/devices/0000:01:00.1/driver/unbind
-
-#echo nvidia > /sys/bus/pci/devices/0000:01:00.0/driver_override
-
-#echo 0000:01:00.0 > /sys/bus/pci/drivers_probe
--- start.sh
+++ start.sh
@@ -2,50 +2,57 @@
 # Helpful to read output when debugging
 set -x
 
+echo "Stopping services..."
 # Stop your display manager. If you're on kde it'll be sddm.service. Gnome users should use 'killall gdm-x-session' instead
+#echo "Stopping KDE display manager..."
 #systemctl --user -M grishi@ stop plasma*
+#echo "Stopping SDDM display manager..."
 #systemctl stop sddm.service
+echo "Stopping nvidia-persistenced.service..."
 systemctl stop nvidia-persistenced.service
+echo "Stopping coolercontrold.service..."
 systemctl stop coolercontrold.service
+echo "Stopping ollama.service..."
 systemctl stop ollama.service
+echo "Stopping comfyui.service..."
 systemctl stop comfyui.service
 
+echo "Checking and killing processes using /dev/nvidia*..."
+if fuser -v /dev/nvidia*; then
+  echo "Killing processes using /dev/nvidia*..."
+  fuser -k /dev/nvidia*
+else
+  echo "No processes are using /dev/nvidia*."
+fi
 
-# Unbind VTconsoles
+#echo "Unbinding VTconsoles..."
 #echo 0 > /sys/class/vtconsole/vtcon0/bind
 #echo 0 > /sys/class/vtconsole/vtcon1/bind
-# Unbind EFI-Framebuffer
+
+#echo "Unbinding EFI-Framebuffer..."
 #echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# Avoid a race condition by waiting a couple of seconds. This can be calibrated to be shorter or longer if required for your system
+echo "Sleeping for 5 seconds to avoid race conditions..."
 sleep 5
 
-# Unload all Nvidia drivers
+echo "Unloading Nvidia drivers..."
 modprobe -r nvidia_uvm
 modprobe -r nvidia_drm
 modprobe -r nvidia_modeset
 modprobe -r i2c_nvidia_gpu
 modprobe -r nvidia
 
-# Unbind the GPU from display driver
+echo "Detaching GPU devices..."
 virsh nodedev-detach pci_0000_01_00_0
 virsh nodedev-detach pci_0000_01_00_1
 
-# Load VFIO kernel module
+echo "Loading VFIO kernel modules..."
 modprobe vfio
 modprobe vfio_pci
 modprobe vfio_iommu_type1
 
+echo "Starting coolercontrold.service..."
 systemctl start coolercontrold.service
 
+#echo "Set LGTV to input 2..."
 #lgtv setInput HDMI_2
-
-##another way to try
-#echo 0000:01:00.0 > /sys/bus/pci/devices/0000:01:00.0/driver/unbind
-#echo 0000:01:00.1 > /sys/bus/pci/devices/0000:01:00.1/driver/unbind
-
-#echo vfio-pci > /sys/bus/pci/devices/0000:01:00.0/driver_override
-#echo vfio-pci > /sys/bus/pci/devices/0000:01:00.1/driver_override
-
-#echo 0000:01:00.0 > /sys/bus/pci/drivers_probe
-#echo 0000:01:00.1 > /sys/bus/pci/drivers_probe
